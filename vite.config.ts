16ae5b2aacc2a2693f5f6457543f270324546e7d
Yahia-dot <yahia.alali.96@gmail.com>
2025-12-21 23:53:19 +0100

    Remove base path from Vite config for HashRouter

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace 'portfolio' with your GitHub repository name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
